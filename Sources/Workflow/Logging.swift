c2bb9322fdb2437d4d3278aad2f8f6c5d14a000c
stefanspringer1 <stefan.springer@icloud.com>
2025-06-16 12:06:41 +0200

    dispense with execution level at message type again

--- Sources/Workflow/Logging.swift
+++ Sources/Workflow/Logging.swift
@@ -138,7 +138,7 @@ public struct LoggingEvent: CustomStringConvertible, Encodable, Sendable {
     
     /// A short textual representation of the logging event.
     public var description: String {
-        return "{\(self.type)}@\(executionLevel.description)\(self.originalType != nil ? " <- \(self.originalType!)" : "")\(self.messageID != nil ? " [\(self.messageID ?? "")]": ""): \(fact[.en]?.trimming() ?? "?")\(solution != nil ? " – solution: \(solution?[.en]?.trimming() ?? "?")" : "")" + (self.itemPositionInfo != nil ? " @ \(self.itemPositionInfo!)" : "")
+        return "{\(self.type)}\(self.originalType != nil ? " <- \(self.originalType!)" : "")\(self.messageID != nil ? " [\(self.messageID ?? "")]": ""): \(fact[.en]?.trimming() ?? "?")\(solution != nil ? " – solution: \(solution?[.en]?.trimming() ?? "?")" : "")" + (self.itemPositionInfo != nil ? " @ \(self.itemPositionInfo!)" : "")
     }
     
     /// A longer textual representation of the logging event used in the actual logging.
